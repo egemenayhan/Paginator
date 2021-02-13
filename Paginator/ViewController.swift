6d6663c2d603cec0b48d78ffc6db7e42d2fc7eac
Egemen Ayhan <egemenayhan@icloud.com>
2021-02-13 11:39:46 +0300

    Initial Commit

---
+++ Paginator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Paginator
+//
+//  Created by Egemen Ayhan on 13.02.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
